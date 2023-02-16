e27629d6b248a575e1bba1bc149ef2db6c34d92a
utente <utente@DESKTOP-LG6PSIP>
2023-02-16 10:18:53 +0100

    mattia

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,43 +1,69 @@
-import java.util.Scanner;
-
-// Chiamare la classe col proprio cognome
-public class MainClass
-{
-	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args)
+import java.util.*;
+public class MainClass {
+	static Scanner in= new Scanner(System.in);
+	public static void main(String[] args) 
 	{
-		while(true)
-		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
-				break;
-				case 2:
-					//Inserire metodo statico
-				break;
-				case 3:
-					//Inserire metodo statico
-				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
+		somma_sottrazione();
+		inversione();
+		inversione_lettere();
 
-			}
+
 		}
+		public static void somma_sottrazione()
+		{
+			System.out.println("quanti numeri scrivere?");
+			int n_scelti = Integer.parseInt(in.nextLine());
+			int numeri2 = 0;
+			int sottrazione_disp = 0;
+			int[] numeri = new int[n_scelti];
+			for (int i = 0; i < n_scelti; i++)
+			{
+				System.out.println("scrivi il numero alla " + (i+1) + "° posizione");
+				numeri[i] = Integer.parseInt(in.nextLine());
+				if (i % 2 == 0)
+				{
+					numeri2 = numeri[i] + numeri2;
+				}
+				else 
+				{
+				sottrazione_disp =- sottrazione_disp - numeri[i];
+				}
+				
+
 	}
-	static void stampaMenu()
-	{
-		// Modificare il menÃ¹ secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
-	}
-	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
+			System.out.println("la somma dei numeri pari è " + numeri2);
+			System.out.println("la differenza dei numeri dispari è " + sottrazione_disp);
+}
+		public static void inversione()
+		{
+			
+			System.out.println("quanti numeri scrivere?");
+			int n_scelti = Integer.parseInt(in.nextLine());		
+			int[] numeri2 = new int[n_scelti];
+			int[] numeri = new int[n_scelti];
+			for (int i = 0; i < n_scelti; i++)
+			{
+				System.out.println("scrivi il numero alla " + (i+1) + "° posizione");
+				numeri[i] = Integer.parseInt(in.nextLine());
+				numeri2[n_scelti -1 - i] = numeri[i];
+			}
+			for (int i = 0; i < n_scelti; i++)
+			System.out.print(numeri2[i] + " ");		
+		}
+		public static void inversione_lettere()
+		{
+			System.out.println(" \nquante lettere vuoi scrivere?");
+			int l_scelta = Integer.parseInt(in.nextLine());
+			String[] lettere = new String[l_scelta];
+			String[] lettere2 = new String[l_scelta];
+			for (int i = 0; i < l_scelta; i++)
+			{
+				System.out.println("scrivi il numero alla " + (i+1) + "° posizione");
+				lettere[i] = in.nextLine();
+				lettere2[l_scelta -1 - i] = lettere[i];
+			}
+			for (int i = 0; i < l_scelta; i++)
+			System.out.print(lettere2[i] + " ");		
+		}
+
 }
